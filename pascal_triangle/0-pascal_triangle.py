ccc9e8193736db48d8b06eff10f4f7ef39ec4e23
Nyiramanzi Igihozo <n.igihozo@alustudent.com>
2026-02-24 10:43:44 -0800

    Add documentation to function

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -5,7 +5,15 @@ Script for pascal's triangles
 
 
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal's triangle of n
 
+    Arguments:
+    n -- number of rows
+
+    Returns:
+    List of lists of integers representing Pascal's triangle of n.
+    """
     if n <= 0:
         return []
 
